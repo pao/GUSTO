c47938c417c14a14af26febb75f114a81952a1db
Patrick O'Leary <patrick.oleary@gmail.com>
2010-03-01 22:44:16 -0600

    Just getting broken console progress dialog implementation out of my way.

---
+++ src/com/olearyp/gusto/ConsoleProgressDialog.java
@@ -0,0 +1,23 @@
+package com.olearyp.gusto;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.view.View;
+
+public class ConsoleProgressDialog extends AlertDialog {
+
+	public ConsoleProgressDialog(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ConsoleProgressDialog(Context context, int theme) {
+		super(context, theme);
+		// TODO Auto-generated constructor stub
+	}
+
+	public void thingy() {
+		View view = null;
+		this.setView(view );
+	}
+}
